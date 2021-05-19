32734601566b64479eaff66a5f66699c3f98bead
11242796 <u11242796@tuks.co.za>
2021-05-19 15:33:14 +0200

    new users

---
+++ Customer_Care_System/Customers/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+              u11242796@tuks.co.za
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
