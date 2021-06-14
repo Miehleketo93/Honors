69d4f6822105e6b4136df266512d667feac54bf9
11242796 <u11242796@tuks.co.za>
2021-06-14 18:22:04 +0200

    change color

--- Customer_Care_System/Staff/configuration/header.php
+++ Customer_Care_System/Staff/configuration/header.php
@@ -3,7 +3,7 @@
                   <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
               </div>
             <!--logo start-->
-            <a href="dashboard.php" class="logo"><b>Customer Care System</b></a>
+            <a href="dashboard.php" class="logo"><b>STAFF PORTAL</b></a>
                 
             <div class="top-menu">
             	<ul class="nav pull-right top-menu">
